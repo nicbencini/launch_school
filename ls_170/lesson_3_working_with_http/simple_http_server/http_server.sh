be36c3f2f42e32b500f95a1291c390a89a9bae4f
nicbencini <nicbencini@gmail.com>
2025-03-10 22:30:16 +0000

    ls170 - networking

--- ls_170/lesson_3_working_with_http/simple_http_server/http_server.sh
+++ ls_170/lesson_3_working_with_http/simple_http_server/http_server.sh
@@ -4,18 +4,33 @@ function server () {
 
     while true
     do
-    read method path version
+        message_arr=()
+    check=true
+    while $check
+    do
+      read line
+      message_arr+=($line)
+      if [[ "${#line}" -eq 1 ]]
+      then
+        check=false
+      fi
+    done
+    method=${message_arr[0]}
+    path=${message_arr[1]}
     if [[ $method = "GET" ]]
+    filepath=./www/$file
     then
-        if [[ -e $path ]]
+        if [[ -f  $filepath ]]
         then
-            echo "HTTP/1.1 200 OK"
+            echo -ne "HTTP/1.1 200 OK\r\n\r\n";cat $filepath
+        else
+            echo -ne "HTTP/1.1 404 Not Found"
+            echo -ne "$filepath Not Found!\r\n\r\n"
+        fi
     else
-        echo "HTTP/1.1 400 Bad Request"
+        echo -ne "HTTP/1.1 400 Bad Request\r\n\r\n"
     fi
     done
-
-
 }
 
 coproc SERVER_PROCESS { server; }
