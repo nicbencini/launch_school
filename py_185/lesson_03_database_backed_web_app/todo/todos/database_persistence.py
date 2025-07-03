b54b484115fa1acc6290ac6b56f04629dedf9057
nicbencini <nicbencini@gmail.com>
2025-07-03 23:49:56 +0100

    py_185: final optimizations

--- py_185/lesson_03_database_backed_web_app/todo/todos/database_persistence.py
+++ py_185/lesson_03_database_backed_web_app/todo/todos/database_persistence.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 class DatabasePersistence:
     def __init__(self):
-        pass
+        self._setup_schema()
     
     @contextmanager
     def _database_connect(self):
@@ -62,7 +62,7 @@ class DatabasePersistence:
                     """)
 
 
-    def _find_todos_for_list(self, list_id):
+    def find_todos_for_list(self, list_id):
         query = "SELECT * FROM todos WHERE list_id = %s"
         logger.info("Executing query: %s with list_id: %s", query, list_id)
         with self._database_connect() as conn:
@@ -82,22 +82,28 @@ class DatabasePersistence:
         
 
         lists = [dict(result) for result in results]
-        for lst in lists:
-            todos = self._find_todos_for_list(lst['id'])
-            lst.setdefault('todos', todos)
+
 
         return lists
 
     def find_list(self, list_id):
-        query = "SELECT * FROM lists WHERE id = %s"
+        query ="""
+            SELECT lists.*,
+                    COUNT(todos.id) AS todos_count,
+                    COUNT(NULLIF(todos.completed, True)) AS todos_remaining
+            FROM lists
+            LEFT JOIN todos ON todos.list_id = lists.id
+            WHERE lists.id = %s
+            GROUP BY lists.id
+            ORDER BY lists.title;
+        """
         logger.info("Executing query: %s with list_id: %s", query, list_id)
         with self._database_connect() as conn:
             with conn.cursor(cursor_factory=DictCursor) as cursor:
                 cursor.execute(query, (list_id,))
                 lst = dict(cursor.fetchone())
         
-        todos = self._find_todos_for_list(list_id)
-        lst.setdefault('todos', todos)
+
         return lst
     
     
